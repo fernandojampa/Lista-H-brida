3e6c887d6c36d3cafc042e45e559ff6c43347e83
Fernando Borges <fernandoborgesjp@gmail.com>
2021-09-06 19:51:14 -0300

    list corrections

--- ListaHibrida.py
+++ ListaHibrida.py
@@ -1,5 +1,5 @@
-from node import Node
 # Classe que vamos usar para tratar as exceções.
+from node import Node
 
 
 class ListaException(Exception):
@@ -7,178 +7,174 @@ class ListaException(Exception):
         super().__init__(msg)
 
 
-class Lista:
+# Inicio da classe Lista, para instanciar as listas como objetos.
 
+class Lista:
     def __init__(self):
+        self._vetor = []
         self._head = None
-        self._tamanho = 0
+
+    @property
+    def vetor(self):
+        return self._vetor
 
     @property
     def head(self):
         return self._head
 
-    @property
-    def tamanho(self):
-        return self._tamanho
+    @vetor.setter
+    def vetor(self, novo):
+        self._vetor = novo
+
+    @head.setter
+    def head(self, novo):
+        self._head = novo
+
+    # Método para checar se a lista está vazia.
 
     def estaVazia(self):
-        return True if self._tamanho == 0 else False
+
+        if not self._vetor:
+            return True
+        else:
+            return False
+
+    # Método para checar o tamanho atual da lista em questão.
 
     def tamanho(self):
-        return self._tamanho
+        print("Total de elementos na lista: ", end='')
+        return (len(self._vetor))
+
+    # Método para consultar o conteúdo de um elemento na lista.
 
     def elemento(self, posicao):
-
         try:
-            assert posicao > 0
             if (self.estaVazia()):
-                raise ListaException('A lista está vazia')
+                raise ListaException("A lista está vazia!")
+
+            cursor = self._head
+            contador = 1
+            while(cursor != None):
+                if(contador == posicao):
+                    return (f'O valor referente ao elemento inserido é: {cursor._carga}')
+                cursor = cursor._prox
+                contador += 1
+        except AttributeError:
+            raise ListaException(
+                'Valor inserido não consta como um elemento presente na lista. Por favor, tente novamente inserindo um número em que haja elemento referente na lista.')
+
+    # Método para buscar um valor na lista.
+
+    def busca(self, valor):
+        try:
+            if (self.estaVazia()):
+                raise ListaException("A lista está vazia!")
 
             cursor = self._head
             contador = 1
 
-            while ((cursor != None) and (contador < posicao)):
-                cursor = cursor.prox
+            while(cursor != None):
+                if(cursor._carga == valor):
+                    return contador
+
+                cursor = cursor._prox
                 contador += 1
 
-            if (cursor != None):
-                return cursor.dado
-
-            raise ListaException('A posiçao é inválida para a lista')
-
-        except TypeError:
+        except AttributeError:
             raise ListaException(
-                'O argumento posição deve ser um valor do tipo inteiro')
-        except AssertionError:
-            raise ListaException('Posição negativa não é válida para a lista')
-        except:
-            raise
+                'Valor inserido não consta como um valor presente na lista. Por favor, tente novamente inserindo um número em que haja elemento referente na lista.')
 
-    def busca(self, valor):
+    # Método para inserir um valor no início da lista.
 
-        if (self.estaVazia()):
-            raise ListaException('A lista está vazia')
+    def inserir_inicio(self, valor):
+        no = Node(valor)
+        if not self._head:
+            self._head = no
+        else:
+            no._prox = self._head
+            self._head = no
+        self._vetor.append(no)
 
-        cursor = self._head
-        contador = 1
-
-        while (cursor != None):
-            if(cursor.dado == valor):
-                return contador
-
-            cursor = cursor.prox
-            contador += 1
-        raise ListaException('O valor informado na busca nao está na lista')
-
-    def inserir_inicio(self, posicao, dado):
-
-        try:
-            assert posicao > 0
-
-            # CONDICAO 1: insercao se a lista estiver vazia
-            if (self.estaVazia()):
-                if (posicao != 1):
-                    print('Entrei no posicao = 1')
-                    raise ListaException(
-                        f'A lista esta vazia. A posicao correta para insercao é 1.')
-
-                self._head = Node(dado)
-                self._tamanho += 1
-                return
-
-            # CONDICAO 2: insercao na primeira posicao em uma lista nao vazia
-            if (posicao == 1):
-                novo = Node(dado)
-                novo.prox = self._head
-                self._head = novo
-                self._tamanho += 1
-                return
-
-        except TypeError:
-            raise ListaException(
-                'O argumento posição deve ser um valor do tipo inteiro')
-        except AssertionError:
-            raise ListaException('Posição negativa não é válida para a lista')
+    # Método para inserir um valor no final da lista.
 
     def inserir_final(self, valor):
-        self.vetor.append(valor)
+        no = Node(valor)
+        if not self._head:
+            self._head = no
+        else:
+            cursor = self._head
+            while cursor != -1:
+                if cursor._prox == -1:
+                    cursor._prox = no
+                    break
+                cursor = cursor._prox
+        self._vetor.append(no)
+
+    # Método para trocar a ordem dos elementos da lista.
 
     def trocar_ordem(self, posicao_elem_1, posicao_elem_2):
         try:
-            assert posicao_elem_1 > 0
+            if posicao_elem_1 <= 0 or posicao_elem_2 <= 0:
+                raise ListaException(
+                    "\nPosição inválida. Por favor, insira uma posição presente na lista, maior que zero.")
+            else:
+                elem_pos_1 = self._vetor[posicao_elem_1-1]
+                elem_pos_2 = self._vetor[posicao_elem_2-1]
+                aux = elem_pos_1._carga
+                elem_pos_1._carga = elem_pos_2._carga
+                elem_pos_2._carga = aux
 
-            if (self.estaVazia()):
-                raise ListaException('Lista vazia. Não é possível remover')
-
-            cursor = self._head
-            contador = 1
-
-            while((cursor != None) and (contador < posicao_elem_1)):
-                cursor = cursor._prox
-                contador += 1
-
-            if(cursor != None):
-                cursor.dado = posicao_elem_2
-                return
-
-            raise ListaException('Posição inválida para a lista')
-
-        except TypeError:
+        except IndexError:
             raise ListaException(
-                'O argumento posição deve ser um valor do tipo inteiro')
-        except AssertionError:
-            raise ListaException('Posição negativa não é válida para a lista')
+                "\nPosição não encontrada. Por favor, insira um valor que esteja presente na lista.")
 
-    def remover_final(self):
-        pass
+    # Método para imprimir os elementos contidos na lista atual.
 
     def imprimir_lista(self):
-        print(self.__repr__())
+        self.__str__()
 
-    def remover_inicio(self, posicao):
-        try:
-            assert posicao > 0
-            if (self.estaVazia()):
-                raise ListaException('Lista vazia. Não é possível remover')
+    # Método para remover um elemento do início da lista.
+    def remover_inicio(self):
+        if (self.estaVazia()):
+            raise ListaException('Não é possível remover de uma lista vazia')
+        else:
+            aux = self._head
+            self._head = self._head._prox
+            self._vetor.remove(aux)
 
+    # Método para remover um elemento do fim da lista.
+    def remover_final(self):
+
+        if (self.estaVazia()):
+            raise ListaException('Não é possível remover de uma lista vazia.')
+        elif len(self._vetor) == 1:
+            self.remove_todos()
+        else:
             cursor = self._head
-            contador = 1
-
-            while((contador <= posicao-1) and (cursor != None)):
-                anterior = cursor
+            while cursor != -1:
+                # Captura o penultimo elemento
+                if cursor._prox._prox == -1:
+                    ultimo = cursor._prox
+                    self._vetor.remove(ultimo)
+                    cursor._prox = -1
+                    break
                 cursor = cursor._prox
-                contador += 1
 
-            if (cursor == None):
-                raise ListaException('Posição inválida para remoção')
-
-            dado = cursor.dado
-
-            if(posicao == 1):
-                self._head = cursor._prox
-
-            else:
-                anterior._prox = cursor._prox
-
-            self._tamanho -= 1
-
-            return dado
-        except TypeError:
-            raise ListaException(
-                'O argumento posição deve ser um valor do tipo inteiro')
-        except AssertionError:
-            raise ListaException('Posição negativa não é válida para a lista')
+    # Método para limpar a lista, removendo todos seus elementos.
 
     def remove_todos(self):
-        self.vetor.clear()
+        self._vetor = []
+        self._head = None
 
     def __str__(self):
-        str = 'Lista: [ '
         cursor = self._head
-        while(cursor != None):
-            str += f'{cursor.dado}'
-            cursor = cursor.prox
-            if(cursor != None):
-                str += ', '
-        str += ']'
-        return str
+        if not self._head:
+            print('[]')
+        else:
+            while cursor != -1:
+                if cursor._prox == -1:
+                    print(f'[{cursor._carga} | -1 ]  \n')
+                else:
+                    print(
+                        f'[{cursor._carga} | {self._vetor.index(cursor._prox)}]  ', end='')
+                cursor = cursor._prox
--- node.py
+++ node.py
@@ -1,13 +1,13 @@
 # Classe Node que é responsável pelo encadeamento das listas
 class Node:
     def __init__(self, dado):
-        self._dado = dado
+        self._carga = dado
         self._prox = -1
 
 
 @property
-def dado(self):
-    return self._dado
+def carga(self):
+    return self._carga
 
 
 @property
@@ -15,15 +15,11 @@ def prox(self):
     return self._prox
 
 
-@dado.setter
-def dado(self, novoDado):
+@carga.setter
+def carga(self, novoDado):
     self._dado = novoDado
 
 
 @prox.setter
 def prox(self, novoProx):
     self._prox = novoProx
-
-
-def __str__(self):
-    return self._dado
